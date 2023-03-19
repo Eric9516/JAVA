2f9056cefd20aed4f8d07539b74377c6afe60b00
Eric9516 <eric-995-@hotmail.com>
2023-03-18 22:22:44 -0300

    TP-1

---
+++ src/tp/pkg1/TP1.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp.pkg1;
+
+/**
+ *
+ * @author usuario
+ */
+public class TP1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        Boolean par = true;
+
+        /*1-d)*/ for (int i = b; i >= a; i--) {
+            if (i % 2 == 0) {
+                System.out.print(i + ", ");
+            }
+
+        }
+    }
+
+}
